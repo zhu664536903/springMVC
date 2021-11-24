a93583cfdf9a7301e4700174243254b012c1204f
gczhu <gczhu185@163.com>
2021-11-24 09:19:27 +0800

    添加异常通知

--- springMVC_annotation/src/main/java/com/gczhu/controller/ExceptionContoller.java
+++ springMVC_annotation/src/main/java/com/gczhu/controller/ExceptionContoller.java
@@ -1,14 +1,18 @@
 package com.gczhu.controller;
 
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class ExceptionContoller  {
     @ExceptionHandler(RuntimeException.class)
     @ResponseBody()
-    public String exp(Exception e){
+    public String exp(Exception e, HttpServletResponse response){
+        response.setContentType("application/json;charset=utf-8");
         return e.getMessage();
     }
 }
